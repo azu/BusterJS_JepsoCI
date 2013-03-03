933a8906ea3d45d1483b7c3536db2b75848f9199
azu <azuciao@gmail.com>
2013-03-03 21:49:18 +0900

    Support jepso-ci

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 Buster.JS with [Jepso CI](https://jepso-ci.com/ "Jepso CI").
+
+
+[![Build Status](https://jepso-ci.com/azu/BusterJS_JepsoCI.svg)](https://jepso-ci.com/azu/BusterJS_JepsoCI)
--- tests/hello-test.js
+++ tests/hello-test.js
@@ -5,7 +5,7 @@
  */
 var assert = buster.assertions.assert;
 buster.testCase("Src Test", {
-    "should be HELLO" : function(){
+    "should be HELLO": function () {
         assert.equals(hello(), "HELLO");
     }
-})
+});
